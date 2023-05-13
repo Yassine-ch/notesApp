a3651e6fd2f1c4bec3514e0ebe1841f0e98e0ff4
Yassine Chebbi <yassine.chebbi2023@gmail.com>
2023-05-14 00:08:53 +0100

    change theme Data

---
+++ android/app/src/main/kotlin/com/yassine/notes_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.yassine.notes_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/notes_view.dart';
+
+void main() {
+  runApp(const NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      //ThemeMode
+theme: ThemeData.dark(),
+      home: const NotesView(),
+
+
+    );
+  }
+}
+
+
